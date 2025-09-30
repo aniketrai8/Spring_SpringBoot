bc8b3710327f1acba0db4312725bfe35b3c411b6
aniketrai8 <rai.aniket@outlook.com>
2025-09-30 10:05:41 +0530

    Student Mangement REST API

---
+++ StudentData/src/main/java/com/example/StudentData/Controllers/StudentController.java
@@ -0,0 +1,47 @@
+package com.example.StudentData.Controllers;
+import com.example.StudentData.Model.Student;
+import com.example.StudentData.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/students")
+
+public class StudentController {
+    private final StudentService studentService;
+
+    // Constructor Injection (best practice)
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Student> getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @PutMapping("/{id}")
+    public Optional<Student> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        return studentService.deleteStudent(id) ? "Deleted" : "Not Found";
+    }
+
+}
